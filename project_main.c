1b86a9f94f4eec11dc6d4d4afedb547ffcc09d55
SouhardyaSengupta <senguptasouhardya19@gmail.com>
2021-04-29 20:02:32 +0530

    Added activity4

--- project_main.c
+++ project_main.c
@@ -10,7 +10,9 @@
  */
 #include "activity1.h"
 #include "activity2.h"
-
+#include "activity3.h"
+#include "activity4.h"
+#define ON 1
 /**
  * @brief Main function where the code execution starts
  * 
@@ -19,16 +21,21 @@
  * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
  */
 int main(void)
-{ 
-	/**
-	 * @brief Construct a new Initialize Peripherals object
-	 *  Initializing Peripherals 
-	 */
-	InitializePeripherals();
-	/**
-	 * @brief 
-	 * Turns LED ON if and only if both switches ButtonSensor and Heater are closed
-	 */
-	StatusOfLedActuator();
+{
+	uint16_t Temperature;
+	uint16_t ADCchannel=0;
+	char TempType;
+	
+	USARTInit(); /* Initialize Peripherals for UART */
+	while(1){
+		uint8_t Status;
+		/* Turns LED ON if and only if both switches ButtonSensor and Heater are closed */
+		Status=StatusOfLedActuator();
+		if(Status==ON){
+			Temperature=ReadADC(ADCchannel); /*reads sensor data from ADCChannel*/
+    	    TempType=GeneratePWM(Temperature);/*Generates PWM according to data received from sensor*/
+			USARTWriteString(TempType);/*Sends data to serial monitor*/
+		}
+	}
 	return 0;
 }
